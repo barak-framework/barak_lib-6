c46a9ebb73ac113723b28e02dbcebdc3dffce10b
Gökhan Demir <gdemir3327@gmail.com>
2020-01-06 09:43:44 +0300

    Create Application.php

---
+++ kernel/Application.php
@@ -0,0 +1,40 @@
+<?php
+
+class Application {
+
+  private static $_configutaion = NULL;
+
+  // application configs sets
+  public static $timezone = "Europe/Istanbul";
+  public static $locale = "tr";
+  public static $debug = false;
+  public static $logger = false;
+
+  // application modules status
+  public static $cacher = false;
+  public static $mailer = false;
+  public static $model = false;
+  public static $http = false;
+
+  public static function set($key, $value) {
+    if (!isset(self::${$key}))
+      throw new Exception("Application yapılandırma dosyasında bilinmeyen parametre → " . $key);
+
+    self::${$key} = $value;
+  }
+
+  public static function config(callable $_functions) {
+    // yapılandırma dosyasını bu fonkiyon ne kadar çağrılırsa çağrılsın sadece bir defa oku!
+    if (!isset(self::$_configutaion)) {
+
+      // config processing
+      $_functions();
+      // config processed
+
+      // bir daha ::config fonksiyonu çağrılmaması için
+      self::$_configutaion = TRUE;
+    }
+  }
+
+}
+?>
