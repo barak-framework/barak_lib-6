a9629ee86fd6befff289abd830ce5c12299d3b7e
Gökhan Demir <gdemir3327@gmail.com>
2020-01-06 13:11:26 +0300

    Update ApplicationDispatcher.php

--- kernel/ApplicationDispatcher.php
+++ kernel/ApplicationDispatcher.php
@@ -19,7 +19,7 @@ class ApplicationDispatcher {
 
       ApplicationLogger::info("Processing by " . ucfirst($route->controller) . "Controller#{$route->action} as HTML");
 
-      // returned status code not including: 404, 500
+      // returned status code | including : 200, 302 | not including: 404, 500
       $response = ApplicationController::get_response($route);
 
     } else {
